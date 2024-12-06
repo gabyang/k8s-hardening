83d9107f1af7f7eb58f678f434954d0d4d99f4d2
gabyang <e0725795@u.nus.edu>
2024-12-07 01:22:35 +0800

    add 2 pages and endpoints

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from datetime import datetime
+from datetime import datetime, timezone
 from pathlib import Path
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
+from fastapi import HTTPException
 
 import uuid
 
@@ -23,7 +24,38 @@ def submit_code_page(request: Request):
 @app.post("/submit-code")
 def submit_code(language: str = Form(...), code: str = Form(...)):
     submission_id = str(uuid.uuid4())
-    submitted_time = datetime.utcnow()
+    submitted_time = datetime.now(timezone.utc)
     db.store(submission_id, language, code, "submitted", submitted_time)
 
     return RedirectResponse(url="/list-code", status_code=303)
+
+
+@app.get("/list-code", response_class=HTMLResponse)
+def list_code(request: Request):
+    records = db.list_all()
+
+    return templates.TemplateResponse(
+        "list.html", {"request": request, "items": records, "count": len(records)}
+    )
+
+
+@app.get("/get-code/{uid}", response_class=HTMLResponse)
+def get_code(uid: str, request: Request):
+    try:
+        record = db.get(uid)
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Record not found")
+
+    return templates.TemplateResponse(
+        "code.html",
+        {
+            "request": request,
+            "ID": record.ID,
+            "Language": record.Language,
+            "Status": record.Status,
+            "Code": record.Code,
+            "Logs": record.Logs,
+            "SubmittedTime": record.SubmittedTime,
+            "CompletedTime": record.CompletedTime,
+        },
+    )
